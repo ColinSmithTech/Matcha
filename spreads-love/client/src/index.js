8117c58f0d9db5d164887c043e0c7eb5d7aa14f5
Maljean <maljean@student.42.us.org>
2019-04-20 15:50:22 -0700

    reboot

--- spreads-love/client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-const App = <div>Hello!</div>;
-
-const container = document.getElementById('root');
-render(
-    App, container
-);
