eea3627e1b6960483bccaf26b303cc684a8ba45f
ColinSmithTech <maljean@student.42.us.org>
2019-04-20 17:09:41 -0700

    enviroment setup

---
+++ matcha/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+    entry: "./client/src/index.js",
+
+    output: {
+        path: path.resolve(__dirname, './client/dist'),
+        filename: 'bundle.js',
+    },
+
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            loader: 'babel-loader',
+            include: [
+                path.resolve(__dirname, './client/src')
+            ],
+            options: {
+                presets: ['react']
+            },
+        }]
+    }
+};
